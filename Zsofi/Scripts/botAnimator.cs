8d21ee1e210cdfbde67025a36caf9e7cbe15af20
nmthzsofi <nemethzsofia442@gmail.com>
2026-05-17 09:03:05 +0200

    cleaning up Assetst folder

--- Zsofi/Scripts/botAnimator.cs
+++ Zsofi/Scripts/botAnimator.cs
@@ -5,23 +5,34 @@ public class botAnimator : MonoBehaviour
 {
     Animator animator;
     float velocity = 0.0f;
+    float direction = 0.0f;
+
     float accelaration = 0.5f;
+    float rotationSpeed = 0.8f;
+
     float deceleration = 0.5f;
 
     int VelocityHash;
+    int DirectionHash;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        VelocityHash = Animator.StringToHash("Velocity");
+        VelocityHash = Animator.StringToHash("Velocity X");
+        DirectionHash = Animator.StringToHash("Velocity Z");
+
     }
 
     void Update()
     {
-        bool forwardPressed = Keyboard.current.rightArrowKey.isPressed;
+        bool forwardPressed = Keyboard.current.upArrowKey.isPressed;
+        bool leftPressed = Keyboard.current.leftArrowKey.isPressed;
+        bool rightPressed = Keyboard.current.rightArrowKey.isPressed;
+
+
         //bool runPressed = Keyboard.current.rightArrowKey.isPressed;
 
-        if (forwardPressed && velocity < 1.0f)
+        if (forwardPressed && velocity < 2.0f)
         {
             velocity += Time.deltaTime * accelaration;
         }
@@ -29,11 +40,20 @@ public class botAnimator : MonoBehaviour
         {
             velocity -= Time.deltaTime * deceleration;
         }
+        if (forwardPressed && rightPressed && !leftPressed && direction < 2.0f)
+        {
+            direction += Time.deltaTime * rotationSpeed;
+        }
+        if (forwardPressed && leftPressed && !rightPressed && direction > -2.0f)
+        {
+            direction -= Time.deltaTime * rotationSpeed;
+        }
         if (!forwardPressed && velocity < 0.1f)
         {
             velocity = 0.0f;
         }
         animator.SetFloat(VelocityHash, velocity, 0.1f, Time.deltaTime);
+        animator.SetFloat(DirectionHash, direction, 0.1f, Time.deltaTime);
 
     }
 }
